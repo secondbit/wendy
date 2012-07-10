4b5ffae98f09b3dfe79939a8882fb421daab712e
Paddy Foran <paddy@secondbit.org>
2012-07-10 06:20:17 -0400

    Initial commit. Added nodeIDs and associated tests. Added gitignore.

---
+++ nodeid.go
@@ -0,0 +1,114 @@
+package pastry
+
+import (
+	"encoding/hex"
+	"errors"
+)
+
+// NodeIDDigit represents a single base 16 digit of a NodeID, stored as a byte (only half of which is used).
+type NodeIDDigit byte
+
+// NodeIDDigitsFromByte creates two NodeIDDigits from a single byte and returns them. The responses should add up to equal the byte that was passed in, as NodeIDDigitsFromByte simply cuts the byte in half using bit-shifting and returns the halves.
+func NodeIDDigitsFromByte(b byte) (NodeIDDigit, NodeIDDigit) {
+	// 0xf = 00001111, 0xf0 = 11110000
+	// b&0xf0 = first four bits, b&0xf = last four bits
+	return NodeIDDigit(b & 0xf0), NodeIDDigit(b & 0xf)
+}
+
+// Canonical returns the NodeIDDigit such that it can be safely compared to other NodeIDDigits by standardising which half of the byte is insignificant.
+func (d NodeIDDigit) Canonical() NodeIDDigit {
+	if d <= 0xf {
+		return d << 4
+	}
+	return d
+}
+
+// String returns the NodeIDDigit encoded as a hexadecimal string with the insignificant half of the byte stripped from the string.
+func (d NodeIDDigit) String() string {
+	asHex := hex.EncodeToString([]byte{byte(d.Canonical())})
+	return string(asHex[0])
+}
+
+// Equals tests two NodeIDDigits for equality, returning true if the digits are considered to be equal and false if they are considered to be inequal. NodeIDDigits are considered to be equal if the significant halves of the bytes that represent them are equal.
+func (d NodeIDDigit) Equals(other NodeIDDigit) bool {
+	return d.Canonical() == other.Canonical()
+}
+
+// Less tests two NodeIDDigits to determine whether the argument is less than the digit the method is being called on. A digit is considered to be less if its significant half of a byte is less than the significant half of the other digit's byte.
+func (d NodeIDDigit) Less(other NodeIDDigit) bool {
+	return d.Canonical() < other.Canonical()
+}
+
+// Diff returns the difference between two NodeIDDigits as an absolute value.
+func (d NodeIDDigit) Diff(other NodeIDDigit) int {
+	if d.Less(other) {
+		return int(uint8(other.Canonical()) - uint8(d.Canonical()))
+	}
+	return int(uint8(d.Canonical()) - uint8(other.Canonical()))
+}
+
+// NodeID is a unique address for a node in the network. It is an array of 32 NodeIDDigits.
+type NodeID []NodeIDDigit
+
+// NodeIDFromBytes creates a NodeID from an array of bytes.
+// It returns the created NodeID, trimmed to the first 32 digits, or nil and an error if there are not enough bytes to yield 32 digits.
+func NodeIDFromBytes(source []byte) (NodeID, error) {
+	var result NodeID
+	if len(source) < 16 {
+		return nil, errors.New("Not enough bytes to create a NodeID.")
+	}
+	for _, b := range source {
+		d1, d2 := NodeIDDigitsFromByte(b)
+		result = append(result, d1, d2)
+	}
+	result = result[:32]
+	return result, nil
+}
+
+// String returns the hexadecimal string encoding of each NodeIDDigit in the NodeID, discarding the insignificant half of the byte.
+func (id NodeID) String() string {
+	result := ""
+	for _, digit := range id {
+		result += digit.String()
+	}
+	return result
+}
+
+// Equals tests two NodeIDs for equality and returns true if they are considered equal, false if they are considered inequal. NodeIDs are considered equal if each digit of the NodeID is equal.
+func (id NodeID) Equals(other NodeID) bool {
+	for i, d := range id {
+		if !d.Equals(other[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Less tests two NodeIDs to determine if the ID the method is called on is less than the ID passed as an argument. An ID is considered to be less if the first inequal digit between the two IDs is considered to be less.
+func (id NodeID) Less(other NodeID) bool {
+	for i, d := range id {
+		if !d.Equals(other[i]) {
+			return d.Less(other[i])
+		}
+	}
+	return false
+}
+
+// CommonPrefixLen returns the number of leading digits that are equal in the two NodeIDs.
+func (id NodeID) CommonPrefixLen(other NodeID) int {
+	for i, d := range id {
+		if !d.Equals(other[i]) {
+			return i
+		}
+	}
+	return len(id)
+}
+
+// Diff returns the difference between two NodeIDs as an absolute value. The difference between two NodeIDs is determined by the difference between the first significant digits of those NodeIDs.
+func (id NodeID) Diff(other NodeID) int {
+	sigdigit := id.CommonPrefixLen(other)
+	if sigdigit < len(id) && sigdigit < len(other) {
+		return id[sigdigit].Diff(other[sigdigit])
+	}
+	return 0
+}
---
+++ nodeid_test.go
@@ -0,0 +1,114 @@
+package pastry
+
+import "testing"
+
+// Make sure the NodeIDDigits returned by NodeIDDigitsFromByte actually add up to equal the original byte.
+func TestNodeIDDigitsFromByteEqualsByte(t *testing.T) {
+	bytes := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-_ ")
+	for _, b := range bytes {
+		d1, d2 := NodeIDDigitsFromByte(b)
+		if uint8(d1)+uint8(d2) != uint8(b) {
+			t.Errorf("%v + %v should equal %v, but instead equals %v.", uint8(d1), uint8(d2), uint8(b), uint8(d1)+uint8(d2))
+		}
+	}
+}
+
+// Make sure an error is thrown if a NodeID is created from less than 32 bytes
+func TestNodeIDFromBytesWithInsufficientBytes(t *testing.T) {
+	bytes := []byte("123456789012345")
+	id, err := NodeIDFromBytes(bytes)
+	if err == nil {
+		t.Errorf("Source length of %v bytes, but no error thrown. Instead returned NodeID of %v", len(bytes), id)
+	}
+}
+
+// Make sure NodeIDDigits discard their insignificant bits when comparing for equality
+func TestNodeIDDigitEqualsDiscardsInsignificantBits(t *testing.T) {
+	d1 := NodeIDDigit(0xf)
+	d2 := NodeIDDigit(0xf0)
+	if !d1.Equals(d2) {
+		t.Errorf("%s should equal %s, but it doesn't.", d1, d2)
+	}
+}
+
+// Make sure the correct difference is reported between NodeIDDigits
+func TestNodeIDDigitDiff(t *testing.T) {
+	d1 := NodeIDDigit(0xf)
+	d2 := NodeIDDigit(0xd0)
+	if d1.Diff(d2) != 32 {
+		t.Errorf("Difference should be 32, was %v instead", d1.Diff(d2))
+	}
+	if d2.Diff(d1) != 32 {
+		t.Errorf("Difference should be 32, was %v instead", d2.Diff(d1))
+	}
+	if d2.Diff(NodeIDDigit(0xd)) != 0 {
+		t.Errorf("Difference should be 0, was %v instead", d2.Diff(NodeIDDigit(0xd)))
+	}
+}
+
+// Make sure an error is *not* thrown if enough bytes are passed in.
+func TestNodeIDFromBytesWithSufficientBytes(t *testing.T) {
+	bytes := []byte("1234567890123456")
+	_, err := NodeIDFromBytes(bytes)
+	if err != nil {
+		t.Errorf("Source length of %v bytes threw an error when no error should have been thrown.", len(bytes))
+		t.Logf(err.Error())
+	}
+}
+
+// Make sure the correct common prefix length is reported for two NodeIDs
+func TestNodeIDCommonPrefixLen(t *testing.T) {
+	n1 := NodeID([]NodeIDDigit{ 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0 })
+	n2 := NodeID([]NodeIDDigit{ 0xf, 0xd0, 0xf, 0xd0, 0xd0, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0 })
+	diff1 := 4
+	
+	n3 := NodeID([]NodeIDDigit{ 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf })
+	n4 := NodeID([]NodeIDDigit{ 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xa, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf })
+	diff2 := 6
+	
+	if n1.CommonPrefixLen(n2) != diff1 {
+		t.Errorf("Common prefix length should be %v, is %v instead.", diff1, n1.CommonPrefixLen(n2))
+		t.Log(n1)
+		t.Log(n2)
+		if len(n1) > n1.CommonPrefixLen(n2) && len(n2) > n1.CommonPrefixLen(n2) {
+			t.Logf("First significant digit: %v vs. %v", n1[n1.CommonPrefixLen(n2)], n2[n1.CommonPrefixLen(n2)])
+		}
+	}
+	if n2.CommonPrefixLen(n3) != 0 {
+		t.Errorf("Common prefix length should be %v, is %v instead.", 0, n2.CommonPrefixLen(n3))
+		t.Log(n2)
+		t.Log(n3)
+		if len(n2) > n2.CommonPrefixLen(n3) && len(n3) > n2.CommonPrefixLen(n3) {
+			t.Logf("First significant digit: %v vs. %v", n2[n2.CommonPrefixLen(n3)], n3[n2.CommonPrefixLen(n3)])
+		}
+	}
+	if n3.CommonPrefixLen(n4) != diff2 {
+		t.Errorf("Common prefix length should be %v, is %v instead.", diff2, n3.CommonPrefixLen(n4))
+		t.Log(n3)
+		t.Log(n4)
+		if len(n3) > n3.CommonPrefixLen(n4) && len(n4) > n3.CommonPrefixLen(n4) {
+			t.Logf("First significant digit: %v vs. %v", n3[n3.CommonPrefixLen(n4)], n4[n3.CommonPrefixLen(n4)])
+		}
+	}
+	if n4.CommonPrefixLen(n4) != len(n4) {
+		t.Errorf("Common prefix length should be %v, is %v instead.", len(n4), n4)
+		if n4.CommonPrefixLen(n4) < len(n4) {
+			t.Logf("First significant digit: %v vs. %v", n4[n4.CommonPrefixLen(n4)], n4[n4.CommonPrefixLen(n4)])
+		}
+	}
+}
+
+// Make sure the correct difference is reported between NodeIDs
+func TestNodeIDDiff(t *testing.T) {	
+	n1 := NodeID([]NodeIDDigit{ 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0 })
+	n2 := NodeID([]NodeIDDigit{ 0xf, 0xd0, 0xf, 0xd0, 0xd0, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0, 0xf, 0xd0 })
+	if n1.Diff(n2) != 32 {
+		t.Errorf("Difference should be 32, was %v instead", n1.Diff(n2))
+	}
+	if n2.Diff(n1) != 32 {
+		t.Errorf("Difference should be 32, was %v instead", n2.Diff(n1))
+	}
+	if n2.Diff(n2) != 0 {
+		t.Errorf("Difference should be 0, was %v instead", n2.Diff(n2))
+	}
+}
