d7e45badc2f83070fb5ad200ebd047e25a35b8ee
Paddy Foran <paddy@secondbit.org>
2012-08-20 23:16:54 -0400

    Updated leafset to actually insert nodes in the proper order. Needs tests still.

--- leafset.go
+++ leafset.go
@@ -19,9 +19,12 @@ type LeafSet struct {
 //
 //The Mode field is used to determine whether the request is to insert, get, or remove the Node from the LeafSet.
 //
+//The Left field designates whether the Node is to the left or the right of the current Node in the NodeID space. True means to the left, false means to the right.
+//
 // Methods that return a leafSetRequest will always do their best to fully populate it, meaning the result can be used to, for example, determine the position of a Node.
 type leafSetRequest struct {
 	Pos  int
+	Left bool
 	Node *Node
 	resp chan *leafSetRequest
 	Mode reqMode
@@ -69,31 +72,45 @@ func (l *LeafSet) insert(r *leafSetRequest) *leafSetRequest {
 	if r.Node == nil {
 		return nil
 	}
+	var pos int
 	side := l.self.ID.RelPos(r.Node.ID)
 	if side == -1 {
-		for i, n := range(l.left) {
-			if n == nil {
-				l.left[i] = r.Node
-				return &leafSetRequest{Mode: mode_set, Node: r.Node, Pos: (-1 * i + 1)}
-			} else {
-				rel := n.ID.RelPos(r.Node.ID)
-				if rel == -1 {
-					// TODO: insert node, push everything left
-				}
-			}
-		}
+		l.left, pos = r.Node.insertIntoArray(l.left, l.self)
+		return &leafSetRequest{Mode: mode_set, Node: r.Node, Pos: pos, Left: true}
 	} else if side == 1 {
-		for i, n := range(l.right) {
-			if n == nil {
-				l.right[i] = r.Node
-				return &leafSetRequest{Mode: mode_set, Node: r.Node, Pos: (i + 1)}
-			} else {
-				rel := n.ID.RelPos(r.Node.ID)
-				if rel == 1 {
-					// TODO: insert node, push everything right
-				}
-			}
-		}
+		l.right, pos = r.Node.insertIntoArray(l.right, l.self)
+		return &leafSetRequest{Mode: mode_set, Node: r.Node, Pos: pos, Left: false}
 	}
 	return nil
+}
+
+// insertIntoArray just inserts the given Node into the array of Nodes such that the nodes will be ordered by ID. It's a helper function for inserting a Node into a LeafSet. It returns an array with the Node inserted and the position of the Node in the new array.
+func (node *Node) insertIntoArray(array [16]*Node, center *Node) ([16]*Node, int) {
+	result := new([16]*Node)
+	result_index := 0
+	src_index := 0
+	inserted := -1
+	for result_index < len(result) {
+		result[result_index] = array[src_index]
+		if inserted >= 0 {
+			continue
+		}
+		if array[src_index] == nil {
+			result[result_index] = node
+			inserted = result_index
+			break
+		}
+		if array[src_index].ID.Equals(node.ID) {
+			inserted = result_index
+			continue
+		}
+		if center.ID.Diff(node.ID).Cmp(center.ID.Diff(result[result_index].ID)) < 0 {
+			result[result_index] = node
+			inserted = result_index
+		} else {
+			src_index += 1
+		}
+		result_index += 1
+	}
+	return result, inserted
 }
