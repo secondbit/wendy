c9b3b81b9fdb4239389eb7c180ac33ace04468fa
Paddy Foran <paddy@secondbit.org>
2012-09-05 00:13:03 -0400

    Basic networking skeleton.

---
+++ cluster.go
@@ -0,0 +1,69 @@
+package pastry
+
+import (
+	"net"
+	"strconv"
+)
+
+// Cluster holds the information about the state of the network. It is the main point of interaction for the network.
+type Cluster struct {
+	self *Node
+	table *RoutingTable
+	leafset *LeafSet
+	req chan *Message
+	kill chan bool
+}
+
+// NewCluster creates a new instance of a connection to the network and all the state tables for it.
+func NewCluster(self *Node) {
+	table := NewRoutingTable(self)
+	leafset := NewLeafSet(self)
+	req := make(chan *Message)
+	kill := make(chan bool)
+	return &Cluster {
+		self: self,
+		table: table,
+		leafset: leafset,
+		req: req,
+		kill: kill
+	}
+}
+
+// Stop shuts down the local listener for the Cluster, preventing it from receiving or sending further messages.
+func (c *Cluser) Stop() {
+	c.table.Stop()
+	c.leafset.Stop()
+	c.kill <- true
+}
+
+func (c *Cluster) listen(port int) {
+	portstr := strconv.Itoa(port)
+	ln, err := net.Listen("tcp", ":" + portstr)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	var msg Message
+	decoder := json.NewDecoder(conn)
+	decoder.Decode(&msg)
+	fmt.Println(msg.Key) // TODO do something useful instead
+}
+
+// Listen starts the Cluster listening for events, including all the individual listeners for each state object.
+func (c *Cluster) Listen() {
+	go c.table.listen()
+	go c.leafset.listen()
+	go c.listen(c.self.Port)
+}
